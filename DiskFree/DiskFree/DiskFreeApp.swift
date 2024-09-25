d16c30d47a364602ef9733da226f690368a1b4d8
Brian Martin <brian.beholden@gmail.com>
2024-09-24 17:16:19 -0700

    initial commit with vanilla new xcode project

---
+++ DiskFree/DiskFree/DiskFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskFreeApp.swift
+//  DiskFree
+//
+//  Created by Brian Martin on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct DiskFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiskFree/DiskFreeTests/DiskFreeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiskFreeTests.swift
+//  DiskFreeTests
+//
+//  Created by Brian Martin on 9/24/24.
+//
+
+import Testing
+@testable import DiskFree
+
+struct DiskFreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
